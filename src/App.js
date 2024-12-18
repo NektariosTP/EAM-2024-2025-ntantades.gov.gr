b57c3ae05bbf58881b52ddb3c91c964b0d0f69ab
Nektarios Pavlakos <nektar.tp@gmail.com>
2024-12-18 16:16:03 +0200

    public & src

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Buttons from "./Buttons";
+import Header  from "./Header";
+import NavBar from "./NavBar";
+
+export default function App() {
+    return (
+      <div className="App">
+        <Header/>
+        <NavBar/>
+        <Buttons/>
+      </div>
+    );
+  }
+  
+
