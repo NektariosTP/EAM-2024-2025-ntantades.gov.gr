eb54cbc7835172b58480cac3434a727febd41971
Nektarios Pavlakos <nektar.tp@gmail.com>
2024-12-25 20:12:11 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import MainPage from "./pages/MainPage/MainPage";
 import Search1 from "./pages/Search1/Search1";
+import Information from "./pages/Information/Information";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/Goneis" element={<Search1 />} />
+          <Route path="/Information" element={<Information />} />
         </Routes>
       </div>
     </Router>
