83490531fb3698d48f2f9da2e29b639f9c41f6d1
Nektarios Pavlakos <nektar.tp@gmail.com>
2024-12-26 18:29:32 +0200

    Add files via upload

--- src/pages/Search1/Search1.js
+++ src/pages/Search1/Search1.js
@@ -1,6 +1,7 @@
 import "./Search1.css";
 import Header  from "../../shared_components/Header";
 import NavBar from "../../shared_components/NavBar";
+import Breadcrumb from "../../shared_components/Breadcrumb";
 import Query from "./Query"
 import Info from "./Info"
 import Reviews from "./Reviews"
@@ -13,6 +14,7 @@ export default function Search1() {
     <div className="Search1">
       <Header/>
       <NavBar/>
+      <Breadcrumb/>
       <Query/>
       <Info/>
       <Reviews/>
