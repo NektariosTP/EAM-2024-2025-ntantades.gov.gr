c12a61a7844a66b6a82068aa3d116a37a08cc621
Nektarios Pavlakos <nektar.tp@gmail.com>
2024-12-26 18:30:23 +0200

    Add files via upload

--- src/pages/Plirofories/Plirofories.js
+++ src/pages/Plirofories/Plirofories.js
@@ -1,6 +1,7 @@
 import "./Plirofories.css";
 import Header  from "../../shared_components/Header";
 import NavBar from "../../shared_components/NavBar";
+import Breadcrumb from "../../shared_components/Breadcrumb";
 import Message from "./Message";
 import NavMenu from "../../shared_components/NavMenu";
 import Footer from "../../shared_components/Footer";
@@ -10,6 +11,7 @@ export default function Plirofories() {
     <div className="Plirofories">
       <Header/>
       <NavBar/>
+      <Breadcrumb/>
       <Message/>
       <NavMenu/>
       <Footer/>
