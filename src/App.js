ccffb594befe50320ba838849cf63945e4880275
Nektarios Pavlakos <nektar.tp@gmail.com>
2024-12-26 17:17:19 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import MainPage from "./pages/MainPage/MainPage";
 import Search1 from "./pages/Search1/Search1";
-import Information from "./pages/Information/Information";
+import Plirofories from "./pages/Plirofories/Plirofories";
+import Epikoinonia from "./pages/Epikoinonia/Epikoinonia";
 
 export default function App() {
   return (
@@ -11,7 +12,8 @@ export default function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/Goneis" element={<Search1 />} />
-          <Route path="/Information" element={<Information />} />
+          <Route path="/Plirofories" element={<Plirofories />} />
+          <Route path="/Epikoinonia" element={<Epikoinonia />} />
         </Routes>
       </div>
     </Router>
