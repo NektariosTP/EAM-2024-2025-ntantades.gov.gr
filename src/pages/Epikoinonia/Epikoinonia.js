2e3b72d1fdfe055c5fb7cc53bbdc6250621387cc
Nektarios Pavlakos <nektar.tp@gmail.com>
2024-12-26 16:39:51 +0200

    Add files via upload

---
+++ src/pages/Epikoinonia/Epikoinonia.js
@@ -0,0 +1,20 @@
+import "./Epikoinonia.css";
+import Header  from "../../shared_components/Header";
+import NavBar from "../../shared_components/NavBar";
+import Address from "./Address";
+import Message from "./Message";
+import NavMenu from "../../shared_components/NavMenu";
+import Footer from "../../shared_components/Footer";
+
+export default function Epikoinonia() {
+  return (
+    <div className="Epikoinonia">
+      <Header/>
+      <NavBar/>
+      <Address/>
+      <Message/>
+      <NavMenu/>
+      <Footer/>
+    </div>
+  );
+}
