09582c985f7955577e5fa826cea99c1aeff26d15
mairacs <126562556+mairacs@users.noreply.github.com>
2025-01-17 23:48:28 +0200

    Add files via upload

--- src/pages/ParentRegistration5/ParentForm5.js
+++ src/pages/ParentRegistration5/ParentForm5.js
@@ -10,7 +10,7 @@ function ParentForm5() {
       <div className="submit-box">
         <p>Είστε σίγουροι ότι θέλετε να ολοκληρώσετε την εγγραφή σας;</p>
         <div className="button-group5-nannies">
-          <button type="button" className="back-button" onClick={() => navigate("/Goneis/Aggelies/Profile")}>
+          <button type="button" className="back-button" onClick={() => window.location.href = `${window.location.origin}/Goneis/Aggelies/Profile`}>
             Ακύρωση Εγγραφής
           </button>
           <button 
@@ -22,14 +22,14 @@ function ParentForm5() {
           >
             Προεπισκόπηση Εγγραφής
           </button>
-          <button type="button" className="submit-button" onClick={() => navigate("/Goneis/Eggrafi/Epitixia_Eggrafis")}>
+          <button type="button" className="submit-button" onClick={() => window.location.href = `${window.location.origin}/Goneis/Eggrafi/Epitixia_Eggrafis`}>
             Ολοκλήρωση Εγγραφής
           </button>
         </div>
       </div>
 
       <div className="button-group5-nannies-back">
-        <button type="button" className="back-button" onClick={() => navigate("/Goneis/Eggrafi/4")}>
+        <button type="button" className="back-button" onClick={() => window.location.href = `${window.location.origin}/Goneis/Eggrafi/4`}>
           Πίσω
         </button>
       </div>
